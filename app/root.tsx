19bf495e461da939537dbca6c85ae76bdca7e7d8
randallnl <159072003+randallnl@users.noreply.github.com>
2026-09-08 14:40:46 -0400

    Refresh styling from Queerlective brand

--- app/root.tsx
+++ app/root.tsx
@@ -25,6 +25,12 @@ export function Layout({ children }: { children: React.ReactNode }) {
 			<head>
 				<meta charSet="utf-8" />
 				<meta name="viewport" content="width=device-width, initial-scale=1" />
+				<link rel="preconnect" href="https://fonts.googleapis.com" />
+				<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+				<link
+					href="https://fonts.googleapis.com/css2?family=Fira+Sans:wght@400;500;600;700;800&family=Nunito:wght@700;800;900&display=swap"
+					rel="stylesheet"
+				/>
 				<Meta />
 				<Links />
 			</head>
